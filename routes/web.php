86cd8e58a639a5f62d16b57364121bc1eeac1b27
Aqeel-Musthaque101 <aqeelmusthaque@gmail.com>
2020-09-18 21:51:39 +0530

    completed crud

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/crud/retail_customer','CRUD\RetailCustomerController@index');
