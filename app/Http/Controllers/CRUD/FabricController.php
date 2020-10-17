27871c0e9b63846584e29c39cfe6fe370d745e27
Af1a1 <aflalahamed0124@gmail.com>
2020-10-18 01:43:36 +0530

    Bug Fix, Readymade Sub Report Generated

---
+++ app/Http/Controllers/CRUD/FabricController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\CRUD\Controllers;
+
+use Illuminate\Http\Request;
+
+class FabricController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/CRUD/FabricSubController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\CRUDC\Controllers;
+
+use Illuminate\Http\Request;
+
+class FabricSubController extends Controller
+{
+    //
+}
