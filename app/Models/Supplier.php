f1323813c8c8c627655eb7b8c88df84a8255e50a
pasinduj97 <pasinduj97@gmail.com>
2020-10-18 15:01:45 +0530

    errors fixed

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -18,6 +18,10 @@ class Supplier extends Model
 
     }
 
+    public function bank(){
+        return $this->belongsTo(Bank::class, "bank_name", "id");
+    }
+
     public function transactions(){
 
         return $this -> hasMany(Transaction::class);
